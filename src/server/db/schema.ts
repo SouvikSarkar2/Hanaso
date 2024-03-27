a62f1660bd9bae8d58116ec5647b793382a3594a
Souvik Sarkar <12345souvik12345@gmail.com>
2024-03-27 23:34:49 +0530

    layout Added

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -46,6 +46,12 @@ export const users = createTable("user", {
     mode: "date",
   }).default(sql`CURRENT_TIMESTAMP`),
   image: varchar("image", { length: 255 }),
+  friends: text("friends")
+    .array()
+    .default(sql`'{}'`),
+  friendRequests: text("friendRequests")
+    .array()
+    .default(sql`'{}'`),
 });
 
 export const conversations = createTable("conversation", {
