5abe46299ea135de962798f6f7921721512b9c8c
Souvik Sarkar <12345souvik12345@gmail.com>
2024-04-11 16:56:39 +0530

    Kafka Added

--- src/app/(main)/chats/_components/Chat2.tsx
+++ src/app/(main)/chats/_components/Chat2.tsx
@@ -40,7 +40,7 @@ import type { Message } from "~/utils/Types";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import { Skeleton } from "~/components/ui/skeleton";
-import { produceMessage } from "~/kafka";
+// import { produceMessage } from "~/kafka";
 const Chat2 = ({
   roomId,
   name,
@@ -153,8 +153,8 @@ const Chat2 = ({
     socket.emit("sendMessage", messageData);
     setMessageList((list) => [...list, messageData]);
     setMessage("");
-    await produceMessage(messageData);
-    // addMessage.mutate(messageData);
+    // await produceMessage(messageData);
+    addMessage.mutate(messageData);
   };
   return (
     <div className="flex h-full w-full flex-col items-center justify-start ">
